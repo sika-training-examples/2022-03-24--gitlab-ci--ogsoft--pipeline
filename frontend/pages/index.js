c657c515255933083ec33985f9ee1412feb39f5f
Ondrej Sika <ondrej@ondrejsika.com>
2022-03-24 14:30:18 +0100

    feat(frontend): Create simple frontend (Next.js app)

---
+++ frontend/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Hello OG Soft!</title>
+      </Head>
+      <h1>Hello OG Soft!</h1>
+    </>
+  );
+}
